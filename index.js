8763bfe698265201af8a7e95f355a6e6980b7cae
Philip Lisak <phil.lisak@gmail.com>
2020-05-27 18:36:51 -0700

    Working on Task 4

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let principal = 200000;
 let interestRate = 0.05;
 let years = 30;
 
-const name = "Phil";
+let name = "Phil";
 
 // 🏡 Task 1.5: Simple Math
 /* To create a monthly mortgage rate calculator, we need to know the number of years in months and the monthly interest rate. 
@@ -47,7 +47,7 @@ function mortgageCalculator() {
     let interestRate = 0.05;
     let years = 30;
 
-    const name = "Phil ";
+    let name = "Phil";
     const monthlyInterestRate = interestRate / 12;
     const periods = years * 12;
     const numerator =
@@ -55,10 +55,10 @@ function mortgageCalculator() {
     const denominator = Math.pow(1 + monthlyInterestRate, periods) - 1;
     const monthlyRate = principal * (numerator / denominator);
 
-    return name + "your monthly rate is " + monthlyRate.toFixed(2);
+    return name + " your monthly rate is " + monthlyRate.toFixed(2);
 }
 
-console.log(mortgageCalculator());
+// console.log(mortgageCalculator());
 
 // 🏡 Task 4: Arguments and Parameters
 /* Substitute the variables in your functions for parameters such that you can substitute `P`, `I`, and `N` when you call the function.
@@ -66,6 +66,24 @@ console.log(mortgageCalculator());
     For example,
     mortgageCalculator(200000, 0.05, 30); <-- should return 1,073.64
     */
+
+function mortgageCalculator(P, I, N) {
+    let principal = 200000;
+    let interestRate = 0.05;
+    let years = 30;
+
+    let name = "Phil";
+    const monthlyInterestRate = I / 12;
+    const periods = N * 12;
+    const numerator =
+        monthlyInterestRate * Math.pow(1 + monthlyInterestRate, periods);
+    const denominator = Math.pow(1 + monthlyInterestRate, periods) - 1;
+    const monthlyRate = P * (numerator / denominator);
+
+    return name + " your monthly rate is " + monthlyRate.toFixed(2);
+}
+
+mortgageCalculator(200000, 0.05, 30);
 
 // 🏡 Task 5: Conditionals
 /* Add another paramter to your function called credit score. This parameter will be a number between 0 and 800 (a credit score).
